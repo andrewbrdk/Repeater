d261a08c603a04c787a6d7eb8da799b7a50d5128
andrew <andrew@debian.dell>
2024-05-01 07:34:05 +0300

    Print output to console

--- main.go
+++ main.go
@@ -1,52 +1,57 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os/exec"
-    "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"time"
 )
 
 type Command struct {
-    Cmd       string `json:"cmd"`
-    Frequency int    `json:"frequency"`
+	Cmd       string `json:"cmd"`
+	Frequency int    `json:"frequency"`
 }
 
 func main() {
-    // Read JSON file
-    jsonFile, err := ioutil.ReadFile("command.json")
-    if err != nil {
-        fmt.Println("Error reading JSON file:", err)
-        return
-    }
+	// Read JSON file
+	jsonFile, err := ioutil.ReadFile("command.json")
+	if err != nil {
+		log.Fatal("Error reading JSON file:", err)
+	}
 
-    // Parse JSON data
-    var cmd Command
-    err = json.Unmarshal(jsonFile, &cmd)
-    if err != nil {
-        fmt.Println("Error parsing JSON:", err)
-        return
-    }
+	// Parse JSON data
+	var cmd Command
+	err = json.Unmarshal(jsonFile, &cmd)
+	if err != nil {
+		log.Fatal("Error parsing JSON:", err)
+	}
 
-    ticker := time.NewTicker(time.Duration(cmd.Frequency) * time.Minute)
-    defer ticker.Stop()
+	ticker := time.NewTicker(time.Duration(cmd.Frequency) * time.Second)
+	defer ticker.Stop()
 
-    for {
-        select {
-        case <-ticker.C:
-            // Execute command
-            executeCommand(cmd.Cmd)
-        }
-    }
+	for {
+		select {
+		case <-ticker.C:
+			// Execute command
+			output, err := executeCommand(cmd.Cmd)
+			if err != nil {
+				log.Println("Error executing command:", err)
+			} else {
+				// Print output to console
+				fmt.Println(output)
+			}
+		}
+	}
 }
 
-func executeCommand(command string) {
-    cmd := exec.Command("/bin/bash", "-c", command)
-    err := cmd.Run()
-    if err != nil {
-        fmt.Println("Error executing command:", err)
-        return
-    }
+func executeCommand(command string) (string, error) {
+	cmd := exec.Command("/bin/bash", "-c", command)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
 }
 
