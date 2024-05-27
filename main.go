4799f1175dea1e579df6a0382bfce412fdd416d5
andrew <andrew.brdk@gmail.com>
2024-05-27 20:32:54 +0300

    wip on moving logic from template to functions

--- main.go
+++ main.go
@@ -254,6 +254,7 @@ func sequenceOnOff(taskidx int, tasks *AMessOfTasks) error {
 	return nil
 }
 
+/*
 const webTasksList = `
 <!DOCTYPE html>
 <html lang="en">
@@ -303,6 +304,41 @@ const webTasksList = `
 </body>
 </html>
 `
+*/
+
+const webTasksList = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasks</title>
+</head>
+<body>
+    {{.HTMLListTasks}}
+    <script>
+        function onoff(task) {
+            fetch('/onoff?task=' + task)
+                .then(response => {
+                    location.reload();
+                })
+                .catch(error => {
+                    console.error('Error toggling state:', error);
+                });
+        }
+        function restart(task, run, cmd) {
+            fetch('/restart??task=' + task + '&run=' + run + '&cmd=' + cmd)
+                .then(response => {
+                    location.reload();
+                })
+                .catch(error => {
+                    console.error('Error restarting task:', error);
+                });
+        }
+    </script>
+</body>
+</html>
+`
 
 type HTMLTemplateData struct {
 	task_idx int
@@ -313,7 +349,27 @@ type HTMLTemplateData struct {
 
 func (t HTMLTemplateData) HTMLListTasks() template.HTML {
 	var sb strings.Builder
+	var btn_text string
 	sb.WriteString("<h1>Tasks</h1>\n")
+	for i, tseq := range t.Mess.Tasks {
+		sb.WriteString("<div>")
+		sb.WriteString("<details open>")
+		sb.WriteString("<summary>")
+		sb.WriteString(fmt.Sprintf("<strong>%s</strong>", tseq.Title))
+		sb.WriteString(fmt.Sprintf("<span>%s</span>", tseq.Cron))
+		if tseq.OnOff {
+			btn_text = "Turn Off"
+		} else {
+			btn_text = "Turn On"
+		}
+		sb.WriteString(fmt.Sprintf("<button onclick=\"onoff( %v )\">%s</button>", i, btn_text))
+		sb.WriteString("</summary>")
+		sb.WriteString("<div style=\"overflow-x:auto;\">")
+		sb.WriteString(t.HTMLHistoryTable(i))
+		sb.WriteString("</div>")
+		sb.WriteString("</details>")
+		sb.WriteString("</div>")
+	}
 	return template.HTML(sb.String())
 }
 
@@ -334,6 +390,36 @@ func (s RunStatus) HTMLStatus() template.HTML {
 	default:
 		return "?"
 	}
+}
+
+func (td HTMLTemplateData) HTMLHistoryTable(task_idx int) string {
+	//todo: move to HTMLTemplateData
+	tseq := td.Mess.Tasks[task_idx]
+	var sb strings.Builder
+	sb.WriteString("<table>\n")
+	for r := -1; r < len(tseq.Tasks); r++ {
+		sb.WriteString("<tr>\n")
+		for c := -1; c <= len(tseq.History); c++ {
+			if r == -1 && c == -1 {
+				sb.WriteString("<th> </th>")
+			} else if r == -1 && c < len(tseq.History) {
+				sb.WriteString(fmt.Sprintf("<th> <a href=\"/?task=%v&run=%v\">%s</a> </th>", task_idx, c, tseq.History[c].Status.HTMLStatus()))
+			} else if r == -1 && c == len(tseq.History) {
+				sb.WriteString("<th>&#9633;</th>")
+			} else if c == -1 {
+				sb.WriteString(fmt.Sprintf("<td> %s </td>", html.EscapeString(tseq.Tasks[r].Name)))
+			} else if c < len(tseq.History) {
+				sb.WriteString(fmt.Sprintf("<td> <a href=\"/?task=%v&run=%v&cmd=%v\">%s</a> </td>", task_idx, c, r, tseq.History[c].Details[r].Status.HTMLStatus()))
+			} else if c == len(tseq.History) {
+				sb.WriteString("<td>&#9633;</td>")
+			} else {
+				slog.Error("this is not supposed to happen")
+			}
+		}
+		sb.WriteString("</tr>\n")
+	}
+	sb.WriteString("</table>\n")
+	return sb.String()
 }
 
 func (tseq TasksSequence) HTMLHistoryTable(task_idx int) template.HTML {
