95439218969c1d25e9cf59ea6ae9a41e0ef5cf92
andrew <andrew.brdk@gmail.com>
2025-02-03 18:41:04 +0300

    WIP streamlit

--- examples/streamlit.py
+++ examples/streamlit.py
@@ -14,10 +14,24 @@ if sidebar_radio == "Wiki Stats":
     st.header("Wiki Stats")
     df = client.query_df("SELECT * FROM repeater.wiki_pageviews")
     st.line_chart(df, x="dt", y="views")#, title="Views")
+    df = client.query_df("SELECT * FROM repeater.wiki_stats")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.line_chart(df, x="dt", y="articles")#, title="Articles")
+    with col2:
+        st.line_chart(df, x="dt", y="edit")#, title="Edits")
+    with col3:
+        st.line_chart(df, x="dt", y="activeusers")#, title="Active Users")
 elif sidebar_radio == "Linux Github Stats":
     st.header("Linux Github Stats")
     df = client.query_df("SELECT * FROM repeater.github_linux_commits_count")
     st.line_chart(df, x="dt", y="commits")#, title="Commits")
+    df = client.query_df("SELECT * FROM repeater.github_linux_stats")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.line_chart(df, x="dt", y="stars")#, title="stars")
+    with col2:
+        st.line_chart(df, x="dt", y="size_kb")#, title="size_kb")
 elif sidebar_radio == "Bitcoin Exchange Rate":
     st.header("Bitcoin Exchange Rate")
     df = client.query_df("SELECT * FROM repeater.btc_exchange_rate")
